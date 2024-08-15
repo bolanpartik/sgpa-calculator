62555ad299397fbe82cbd9fe011edd2914e3693d
Partik Bolan <bolanpartik@gmail.com>
2024-08-15 18:42:55 +0530

    credit validation

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const creditInput = document.getElementById("input-credit");
 const subjectInput = document.getElementById("input-subject")
 const addSubjectBtn = document.getElementById("add-subject");
 const showOuterContainer = document.getElementById("outer-show-container")
-const clearInputs=document.getElementById("clear-inputs");
+const clearInputs = document.getElementById("clear-inputs");
 
 let mode = "dark";
 let calBtnExists = false
@@ -38,16 +38,16 @@ const changeMode = () => {
 }
 
 //  To show the final sgpa on screen
-const showSPGA=(sgpa)=>{
-    const showFinalResult=document.createElement("div")
+const showSPGA = (sgpa) => {
+    const showFinalResult = document.createElement("div")
     showFinalResult.classList.add('final-result')
 
-    const resultSGPA=document.createElement('p')
+    const resultSGPA = document.createElement('p')
     resultSGPA.classList.add('result-sgpa')
-    resultSGPA.innerText=`Your SPGA is : ${sgpa}`;
+    resultSGPA.innerText = `Your SPGA is : ${sgpa}`;
 
-    const cancelButton=document.createElement('button')
-    cancelButton.innerText='Cancel'
+    const cancelButton = document.createElement('button')
+    cancelButton.innerText = 'Cancel'
     cancelButton.classList.add('cancel-button')
 
     showFinalResult.appendChild(resultSGPA)
@@ -55,31 +55,31 @@ const showSPGA=(sgpa)=>{
 
     document.body.appendChild(showFinalResult)
     document.getElementsByTagName('main')[0].classList.add('non-active')
-    
-    cancelButton.addEventListener('click',()=>{
+
+    cancelButton.addEventListener('click', () => {
         document.body.removeChild(showFinalResult)
         document.getElementsByTagName('main')[0].classList.remove('non-active')
     })
 }
 
 // Round off value up to two decimals
-const roundOffValue=(num)=>{
-    return Math.round(num*100)/100
+const roundOffValue = (num) => {
+    return Math.round(num * 100) / 100
 }
 
 // Covert grades in points
-const gradeToPoints=(grade)=>{
-    switch(grade){
-        case 'A+' : return 10
-        case 'A' : return 9
-        case 'B+' : return 8
-        case 'B' : return 7
-        case 'C+' : return 6
-        case 'C' : return 5
-        case 'D' : return 4
-        case 'E' : return 0
-        case 'F' : return 0
-        default : return 0
+const gradeToPoints = (grade) => {
+    switch (grade) {
+        case 'A+': return 10
+        case 'A': return 9
+        case 'B+': return 8
+        case 'B': return 7
+        case 'C+': return 6
+        case 'C': return 5
+        case 'D': return 4
+        case 'E': return 0
+        case 'F': return 0
+        default: return 0
     }
 }
 
@@ -90,12 +90,12 @@ const calculateAll = () => {
     const allSubjects = document.getElementsByClassName("show-container")
     for (i = 0; i < allSubjects.length; i++) {
         const credit = parseFloat(allCredits[i])
-        const grade =gradeToPoints(allGrades[i])
-        sum +=credit*grade
+        const grade = gradeToPoints(allGrades[i])
+        sum += credit * grade
         totalCredit += credit
     }
     const userSGPA = sum / totalCredit;
-    const roundOFFSgpa=roundOffValue(userSGPA);
+    const roundOFFSgpa = roundOffValue(userSGPA);
     showSPGA(roundOFFSgpa)
 }
 
@@ -106,12 +106,12 @@ const checkTotalSubjects = () => {
         const calculateBtn = document.createElement('button')
         calculateBtn.classList.add('calculate-button')
         calculateBtn.innerText = "Calculate"
-        calculateBtn.addEventListener("click", calculateAll) 
+        calculateBtn.addEventListener("click", calculateAll)
         showOuterContainer.appendChild(calculateBtn)
         calBtnExists = true;
-    }else if(totalSubjects<=1 && calBtnExists){
-            showOuterContainer.removeChild(document.getElementsByClassName('calculate-button')[0])
-            calBtnExists=false
+    } else if (totalSubjects <= 1 && calBtnExists) {
+        showOuterContainer.removeChild(document.getElementsByClassName('calculate-button')[0])
+        calBtnExists = false
     }
 }
 
@@ -145,7 +145,7 @@ const createSubject = () => {
     deleteBtn.classList.add('delete-button')
     deleteBtn.innerText = "Delete"
 
-    card.append(subject,credit,grade,deleteBtn)
+    card.append(subject, credit, grade, deleteBtn)
 
     deleteBtn.addEventListener('click', () => {
         showOuterContainer.removeChild(card)
@@ -159,20 +159,28 @@ function checkVaild(input) {
     return input.value !== null && input.value !== ''
 }
 
+// Fuction to check credit is positive integer or not
+function isCreditValid(credit) {
+    return credit.value > 0
+}
+
 switchModes.addEventListener("click", changeMode)
 
-clearInputs.addEventListener('click',resetInputs)
+clearInputs.addEventListener('click', resetInputs)
 
 addSubjectBtn.addEventListener("click", () => {
     if (checkVaild(subjectInput) && checkVaild(gradeInput) && checkVaild(creditInput)) {
-        allCredits.push(creditInput.value)
-        allGrades.push(gradeInput.value)
-        createSubject()
-        resetInputs()
-        checkTotalSubjects()
+        if (isCreditValid(creditInput)) {
+            allCredits.push(creditInput.value)
+            allGrades.push(gradeInput.value)
+            createSubject()
+            resetInputs()
+            checkTotalSubjects()
+        } else {
+            alert("Credit must be a positive integer")
+            creditInput.focus()
+        }
     } else {
         alert("Grade or Credit or Subject must be enter.")
     }
 })
-
-// commit - show final result
